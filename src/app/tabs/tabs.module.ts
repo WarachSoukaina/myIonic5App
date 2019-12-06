0a3cabdf215e361c62d31b981f44a9ea3767f0da
wsoukaina <w_soukaina@hotmail.fr>
2019-12-06 13:37:16 +0100

    first

--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -6,14 +6,17 @@ import { FormsModule } from '@angular/forms';
 import { TabsPageRoutingModule } from './tabs-routing.module';
 
 import { TabsPage } from './tabs.page';
+import {NewsPageModule} from '../news/news.module'
 
 @NgModule({
+
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
-    TabsPageRoutingModule
-  ],
+    TabsPageRoutingModule,
+    NewsPageModule
+    ],
   declarations: [TabsPage]
 })
 export class TabsPageModule {}
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: false,
+  apiUrl: 'https://newsapi.org/v2/',
+  apiKey: '9380088c4f374c8981b3cdf5687a9aff'
+
 };
